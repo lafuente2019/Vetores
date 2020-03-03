c59f4d2873f70fd4920b294b6763872cf639ca6d
valter lafuente junior <valterlafuente@yahoo.com.br>
2020-03-02 21:02:20 -0300

    acrescentado mais funçoes de manipulação

--- src/vetores/manipulacaoVetores.java
+++ src/vetores/manipulacaoVetores.java
@@ -24,9 +24,9 @@ public class manipulacaoVetores {
         Scanner ler = new Scanner(System.in);
         for (int i = 0; i < vetor.length; i++) {
             System.out.print("Vetor [" + i + "]:");
-            vetor[i] = ler.nextInt(); 
+            vetor[i] = ler.nextInt();
         }
-          System.out.println("-----------------------");
+        System.out.println("-----------------------");
     }
 
     public static int Maior(int v[], int elementos) {
@@ -58,7 +58,7 @@ public class manipulacaoVetores {
     }
 
     public static void intercalaVetor(int v[], int[] v1, int v2[]) {
-   
+
         int aux = 0;
         for (int i = 0; i < v.length; i++) {
             v2[aux] = v[i];
@@ -70,52 +70,72 @@ public class manipulacaoVetores {
             v2[aux] = v1[i];
             aux += 2;
         }
-       System.out.println("----Criando o vetor C com a soma "
-               + "do tamanhos de A e B -----");
-       for (int i = 0; i < v2.length; i++) {
-        System.out.println("vetor[" + i + "]" + v2[i]);
-      }
+        System.out.println("----Criando o vetor C com a soma "
+                + "do tamanhos de A e B -----");
+        for (int i = 0; i < v2.length; i++) {
+            System.out.println("vetor[" + i + "]" + v2[i]);
+        }
         System.out.println("----Ordem crescente-----");
-       int p;
-       for (int i = 0; i < v2.length ; i++) {
-            for (int j = 0 ; j < v2.length-1 ; j++) {
-                if(v2[i] < v2[j]){
-                 p = v2[i];
-                 v2[i] = v2[j];
-                 v2[j] = p; 
-                }  
-            }    
+        int p;
+        for (int i = 0; i < v2.length; i++) {
+            for (int j = 0; j < v2.length - 1; j++) {
+                if (v2[i] < v2[j]) {
+                    p = v2[i];
+                    v2[i] = v2[j];
+                    v2[j] = p;
+                }
+            }
         }
         for (int i = 0; i < v2.length; i++) {
-        System.out.println("vetor[" + i + "]" + v2[i]);
-      }
+            System.out.println("vetor[" + i + "]" + v2[i]);
+        }
     }
+
     public static boolean buscar(int v[], int entrada) {
 
         for (int i = 0; i < v.length; i++) {
             if (v[i] == entrada) {
-             return true; 
-            } 
-           }
-        return false;
-    } 
-    public static void rearranja(int []v){
-          int p;
-       for (int i = 0; i < v.length ; i++) {
-            for (int j = 0 ; j < v.length-1 ; j++) {
-                if(v[i] < v[j]){
-                 p = v[i];
-                 v[i] = v[j];
-                 v[j] = p; 
-                }  
-            }    
+                return true;
+            }
         }
-        for (int i = 0; i < v.length; i++) {
-        System.out.println("vetor[" + i + "]" + v[i]);
-      }
+        return false;
     }
+
+    private static void trocar(int[] v, int i, int j) {
+        int aux = v[i];
+        v[i] = v[j];
+        v[j] = aux;
+    }
+
+    private static int separar(int[] v, int esq, int dir) {
+        int i = esq + 1;
+        int j = dir;
+        int pivo = v[esq];
+        while (i <= j) {
+            if (v[i] <= pivo) {
+                i++;
+            } else if (v[j] > pivo) {
+                j--;
+            } else if (i <= j) {
+                trocar(v, i, j);
+                i++;
+                j--;
+            }
+        }
+        trocar(v, esq, j);
+        return j;
+    }
+
+    public static void quicksort(int[] v, int esq, int dir) {
+        if (esq < dir) {
+            int j = separar(v, esq, dir);
+            quicksort(v, esq, j - 1);
+            quicksort(v, j + 1, dir);
+        }
+    }
+
     public static void main(String[] args) {
-        /*
+        
         int tm = tamanhoVetor();
         int tm1 = tamanhoVetor();
         int[] v = new int[tm];
@@ -124,13 +144,16 @@ public class manipulacaoVetores {
         populaVetor(v1);
         int[] v2 = new int[tm + tm1];
 
-        intercalaVetor(v, v1, v2);*/
+        intercalaVetor(v, v1, v2);
         
-        int tm = tamanhoVetor();
-        int[] v2 = new int[tm];
-        populaVetor(v2);
-        
-        rearranja(v2);
-    }
-   }
+        System.out.println("------Rearranjo------");
+        int tm3 = tamanhoVetor();
+        int[] v3 = new int[tm3];
+        populaVetor(v3);
 
+        quicksort(v3, 0, v3.length-1);
+        for (int i = 0; i < v3.length; i++) {
+            System.out.println("Vetor[" + i + "]:" + v3[i]);
+    }
+  }
+}
